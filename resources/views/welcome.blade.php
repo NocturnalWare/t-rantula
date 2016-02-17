86125df8d0001d753eb76bcb378f68c1b9bf4744
Jota Vares <joe@eternallynocturnal.com.com>
2016-02-17 13:23:38 -0500

    Original Commit
    Master

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,33 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Manage.Band</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Font Awesome -->
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
+<link href='https://fonts.googleapis.com/css?family=Cambay' rel='stylesheet' type='text/css'>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+    
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    
+</head>
+    <body style="background-color:#000">
+        <div class="col-sm-4 col-sm-offset-4">
+            <img class="img-responsive" src="http://localhost/t-rantula/public/images/painting.png">
+        </div>
+        <div class="col-sm-4 col-sm-offset-4" style="color:#fff">
+            <h4 style="text-align:center">
+                <div class="row">
+                   I'm Tori Ranta. This is an original reproduction on canvas.
+                </div>
+                <div class="row" style="padding-top:15px;">
+                    Buy it now for <u>$100.00</u>
+                </div>
+            </h4>
+        </div>
+    </body>
+</html>
